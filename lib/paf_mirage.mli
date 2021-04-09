291f3e271b6fc0ddbfee08309e0e1e85310268fe
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-04-09 14:10:21 +0200

    .

--- lib/paf_mirage.mli
+++ lib/paf_mirage.mli
@@ -1,11 +1,19 @@
 module type S = sig
   type stack
 
+  module TCP : Mirage_flow.S
+
+  module TLS : module type of Tls_mirage.Make (TCP)
+
   type t
 
   type dst = Ipaddr.t * int
 
   val init : port:int -> stack -> t Lwt.t
+
+  val accept : t -> (TCP.flow, [> `Closed ]) result Lwt.t
+
+  val close : t -> unit Lwt.t
 
   val http_service :
     ?config:Httpaf.Config.t ->
@@ -23,6 +31,8 @@ module type S = sig
   val serve :
     ?stop:Lwt_switch.t -> 't Paf.service -> 't -> [ `Initialized of unit Lwt.t ]
 
+  val tcp_protocol : (stack * Ipaddr.t * int, TCP.flow) Mimic.protocol
+
   val tcp_edn : (stack * Ipaddr.t * int) Mimic.value
 
   val tls_edn :
@@ -33,6 +43,11 @@ module type S = sig
     * int)
     Mimic.value
 
+  val tls_protocol :
+    ( [ `host ] Domain_name.t option * Tls.Config.client * stack * Ipaddr.t * int,
+      TLS.flow )
+    Mimic.protocol
+
   val run :
     ctx:Mimic.ctx ->
     error_handler:(dst option -> Alpn.client_error -> unit) ->
@@ -42,4 +57,4 @@ module type S = sig
 end
 
 module Make (Time : Mirage_time.S) (Stack : Mirage_stack.V4V6) :
-  S with type stack = Stack.t
+  S with type stack = Stack.t and type TCP.flow = Stack.TCP.flow
